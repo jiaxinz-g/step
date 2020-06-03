b3e7c71589b455a6253179e9078d821c62f3f260
Jiaxin Zhang <jiaxinz@google.com>
2020-06-03 06:05:24 +0000

    Add a comment input box and display all comments have been left on the site.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,42 @@
 
 package com.google.sps.servlets;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private List<String> comments = new ArrayList<>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Jiaxin!");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);  
+
+    response.getWriter().println(json);
   }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    comments.add(getComment(request));
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
+  }
+
+   /** Returns the comment from user's input. */
+  private String getComment(HttpServletRequest request) {
+    return request.getParameter("comment");  
+  }
+
 }
